1c57ad3a0abe605b0d3b64700e753a2b3530aa67
Silknes <eliott.dupau@gmail.com>
2018-05-10 18:20:09 +0200

    Link Main Activity Layout's elements in MainActivity

--- app/src/main/java/com/eliott/android/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/eliott/android/moodtracker/Controller/MainActivity.java
@@ -2,14 +2,23 @@ package com.eliott.android.moodtracker.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.eliott.android.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView mMoodImage;
+    private ImageButton mCommentButton;
+    private ImageButton mHistoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoodImage = (ImageView) findViewById(R.id.main_activity_mood_img);
+        mCommentButton = (ImageButton) findViewById(R.id.main_activity_comment_btn);
+        mHistoryButton = (ImageButton) findViewById(R.id.main_activity_history_button);
     }
 }
