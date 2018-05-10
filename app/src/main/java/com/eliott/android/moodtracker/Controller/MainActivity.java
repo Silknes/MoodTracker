8003b572d8d8bb8bbe3893198f8a2f559dc62c1e
Silknes <eliott.dupau@gmail.com>
2018-05-10 20:39:13 +0200

    To Ignore

--- app/src/main/java/com/eliott/android/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/eliott/android/moodtracker/Controller/MainActivity.java
@@ -1,7 +1,8 @@
 package com.eliott.android.moodtracker.Controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -13,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mCommentButton;
     private ImageButton mHistoryButton;
 
-    private Mood[] mMood;
+    private Mood[] mMoods;
+    private Mood mMood;
+
+    private int mIndexOfMood = 3;
+
+    private View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,16 @@ public class MainActivity extends AppCompatActivity {
         mMoodImage = (ImageView) findViewById(R.id.main_activity_mood_img);
         mCommentButton = (ImageButton) findViewById(R.id.main_activity_comment_btn);
         mHistoryButton = (ImageButton) findViewById(R.id.main_activity_history_button);
+
+        mView = (View) findViewById(R.id.main_activity_layout);
+        initMoods();
+        mMood = mMoods[mIndexOfMood];
+        mView.setBackgroundColor(mMood.getColor());
+
     }
 
-    public void initMood(){
-        mMood = new Mood[]{
+    public void initMoods(){
+        mMoods = new Mood[]{
                 new Mood(R.color.faded_red, R.drawable.smiley_sad),
                 new Mood(R.color.warm_grey, R.drawable.smiley_disappointed),
                 new Mood(R.color.cornflower_blue_65, R.drawable.smiley_normal),
@@ -34,4 +46,10 @@ public class MainActivity extends AppCompatActivity {
                 new Mood(R.color.banana_yellow, R.drawable.smiley_super_happy),
         };
     }
+
+    /*
+    public Mood getMoods(int mIndexOfMood){
+        return mMoods[mIndexOfMood];
+    }
+    */
 }
